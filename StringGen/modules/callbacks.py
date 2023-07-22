cb12007bf6e77adbbe71049a71bd69b87ad2cd76
meliodas560 <134010249+meliodas560@users.noreply.github.com>
2023-07-23 04:37:30 +0700

    update StringGen/modules/callbacks.py

--- StringGen/modules/callbacks.py
+++ StringGen/modules/callbacks.py
@@ -14,7 +14,7 @@ async def cb_choose(_, cq: CallbackQuery):
     query = cq.matches[0].group(1)
     if query == "gensession":
         return await cq.message.reply_text(
-            text="<b>» ᴄʟɪᴄᴋ ᴏɴ ᴛʜᴇ ʙᴜᴛᴛᴏɴs ʙᴇʟᴏᴡ ғᴏʀ ɢᴇɴᴇʀᴀᴛɪɴɢ ʏᴏᴜʀ sᴇssɪᴏɴ :</b>",
+            text="<b>» Klik tombol untuk membuat String :</b>",
             reply_markup=gen_key,
         )
     elif query.startswith("pyrogram") or query.startswith("telethon"):
